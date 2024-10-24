226f5eaf38b2ed133466bf311f5e57a152355731
Henry Jacob <sojack108@gmail.com>
2024-10-24 11:54:55 +0530

    select queries 3
    select queries from 11-15 added

---
+++ SQL/Basic Select/Weather Observation Station 10.sql
@@ -0,0 +1,36 @@
+/*
+| Mode  | Category       | Max Score | Author       | Link                                                                                             |
+|-------|----------------|-----------|--------------|--------------------------------------------------------------------------------------------------|
+| Easy  | SQL (Basic)    | 10        | Henry Jacob  | https://www.hackerrank.com/challenges/weather-observation-station-10/problem?isFullScreen=true   |
+
+
+Problem
+
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
+| Field Type | Description   |
+|------------|---------------|
+| ID         | NUMBER        |
+| CITY       | VARCHAR2(21)  |
+| STATE      | VARCHAR2(2)   |
+| LAT_N      | NUMBER        |
+| LONG_W     | NUMBER        |
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+-- Solution
+-- MS SQL Server
+
+SELECT DISTINCT
+    CITY
+FROM
+    STATION
+WHERE
+    LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+    
